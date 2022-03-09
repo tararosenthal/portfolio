7696a0f66b6eb3dea2949c65468415ac96838f3e
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2022-03-09 16:07:22 -0600

    Fix for browsers versions that do not support webp

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,37 @@
 
 })(jQuery);
 
+// check_webp_feature:
+//   'feature' can be one of 'lossy', 'lossless', 'alpha' or 'animation'.
+//   'callback(feature, result)' will be passed back the detection result (in an asynchronous way!)
+function check_webp_feature(feature, callback) {
+    var kTestImages = {
+        lossy: "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA",
+        lossless: "UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA==",
+        alpha: "UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAARBxAR/Q9ERP8DAABWUDggGAAAABQBAJ0BKgEAAQAAAP4AAA3AAP7mtQAAAA==",
+        animation: "UklGRlIAAABXRUJQVlA4WAoAAAASAAAAAAAAAAAAQU5JTQYAAAD/////AABBTk1GJgAAAAAAAAAAAAAAAAAAAGQAAABWUDhMDQAAAC8AAAAQBxAREYiI/gcA"
+    };
+    var img = new Image();
+    img.onload = function () {
+        var result = (img.width > 0) && (img.height > 0);
+        callback(feature, result);
+    };
+    img.onerror = function () {
+        callback(feature, false);
+    };
+    img.src = "data:image/webp;base64," + kTestImages[feature];
+}
+
+window.addEventListener("load", function(){
+	check_webp_feature("lossy", assignSupportedImageType);
+});
+
+function assignSupportedImageType(feature, result) {
+	if (!result) {
+		document.documentElement.style.setProperty("--header-img-url", "url(images/header.jpg)");
+	}
+}
+
 window.addEventListener("load", function() {
 	const form = document.getElementById('contact-form');
 	form.addEventListener("submit", function(e) {
